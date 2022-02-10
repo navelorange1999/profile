cf4a4a8134988b60a5401f24b98ac049678b49ef
q.chen <qichen1999@qq.com>
2022-02-10 22:48:55 +0800

    refactor: 💡 Try to run github action

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -6,9 +6,14 @@ CURRENT_MASTER_HEAD_HASH=$(git rev-parse --short HEAD)
 yarn build
 BUILD_DATE=$(date +%F' '%H:%M:%S)
 
-# git remote add origin git:${ACCESS_TOKEN}@github.com:navelorange1999/profile.git
+# cd dist
+
+# git init
 git config user.name "Custom-Github-Action-Bot by @navelorange1999"
 git config user.email "qichen1999@qq.com"
+git subtree pull --prefix dist --squash origin gh-pages 
 git add dist && git commit -m ":rocket: Devlop from ${CURRENT_MASTER_HEAD_HASH}. Build at ${BUILD_DATE}"
-
 git subtree push --prefix dist origin gh-pages
+# git push git:${ACCESS_TOKEN}@github.com:navelorange1999/profile.git master:gh-pages -f
+
+# cd -
